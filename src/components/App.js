4ebac8447aae5da53a598df2e25477133dfc2b0f
Jackmc <jackmc1122@gmail.com>
2019-08-19 13:43:56 -0700

    Add name tag container component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
+import NameTag from '../containers/NameTag';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <NameTag />;
 }
-  
---
+++ src/containers/NameTag.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class NameTag extends React.Component {
+
+  render() {
+    return <p>Name Tag</p>; 
+  }
+}
+
+export default NameTag;
